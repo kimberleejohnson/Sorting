378a12eea2ab54b145c473e3b4db19289f1d47de
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-09-09 16:31:27 -0700

    Added comments to selection sort and definition to both sorts

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,18 +1,26 @@
 # Selection_sort() function 
+# Selection sort repeatedly finds the smallest element 
+
 def selection_sort( arr ):
     # Loop through n-1 elements
     for i in range(0, len(arr) - 1):
+        
         # Find next smallest element, with another loop starting at the index one after i 
         for j in range(i + 1, len(arr)):
+            
             # If the value at i is greater than the value at j, j is the new smallest
             if arr[i] > arr[j]:
-                # Swap the new smallest index with the first 
+                
+                # Swap values 
                 arr[i], arr[j] = arr[j], arr[i]
+    
     return arr
 
 
-# Bubble Sort function below
+# Bubble Sort function
+# Bubble Sort repeatedly swaps adjacent elements if they are in the wrong order 
 def bubble_sort( arr ):
+
     complete = False 
     while not complete: 
         swapped = False 
