7e4413d4f572a7d2712cbe2a0b7b14841bf1868b
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-09-09 16:35:43 -0700

    Added comments to bubble sort

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -21,15 +21,28 @@ def selection_sort( arr ):
 # Bubble Sort repeatedly swaps adjacent elements if they are in the wrong order 
 def bubble_sort( arr ):
 
+    # Set Boolean value for while loop  
     complete = False 
+
+    # While Boolean complete is NOT true 
     while not complete: 
+
+        # Another Boolean for whether values need to be swapped 
         swapped = False 
+
+        # For i in range of first value, and length of array 
         for i in range(1, len(arr)): 
+
+            # If the value before arr[i] is greater than arr[i]
             if arr[i - 1] > arr[i]:
+
+                # Swap the values 
                 temp = arr[i]
                 arr[i] = arr[i - 1]
                 arr[i - 1] = temp
                 swapped = True 
+        
+        # If you don't need to swap the values 
         if not swapped: 
             complete = True 
     return arr 
